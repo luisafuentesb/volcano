dfb41395a3f2d1d3080b3742828b275442989704
luisafuentesb <112406796+luisafuentesb@users.noreply.github.com>
2022-09-11 09:13:58 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,86 @@
+// Magma rising through a vent
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . . . #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(250)
+}
+// Lava erupting 
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # . # . #
+        . . # . .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        . # # # .
+        . . . . .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(250)
+}
